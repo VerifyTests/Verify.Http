2421c96ff636e4bc3a39e927ce0cab7b6c69fde7
Simon Cropp <simon.cropp@gmail.com>
2024-01-14 18:51:14 +1100

    Update HttpResponseSplitterResult.cs

--- src/Verify.Http/HttpResponseSplitterResult.cs
+++ src/Verify.Http/HttpResponseSplitterResult.cs
@@ -6,7 +6,7 @@
         var content = instance.Content;
         if (content.TryGetExtension(out var extension))
         {
-            if (FileExtensions.IsText(extension))
+            if (FileExtensions.IsTextExtension(extension))
             {
                 return new(instance, targets);
             }
